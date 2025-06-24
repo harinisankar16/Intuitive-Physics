a529a0ebabd65937c3fc8aa36fd69624bebf0c20
Harini Sankar <hsankar3@illinois.edu>
2025-06-24 14:31:09 +0530

    first upload

---
+++ create_tower.py
@@ -0,0 +1,52 @@
+from ursina import *
+import random
+
+app = Ursina()
+
+EditorCamera()
+
+
+# Create a ground/base
+ground = Entity(model="cube", color=color.gray, scale=(6, 0.1, 6), position=(0, 0, 0))
+
+# Define block dimensions
+block_width = 1
+block_height = 0.5
+block_depth = 1
+
+# Number of blocks in the tower
+num_blocks = 10
+
+blocks = []
+
+# Build the tower
+for i in range(num_blocks):
+
+    x_jitter = random.uniform(
+        -block_width / 2, block_width / 2
+    )  # randomly jitter the x position for each cube
+    z_jitter = random.uniform(
+        -block_depth / 2, block_depth / 2
+    )  # randomly jitter the z position for each cube
+
+    block = Entity(
+        model="cube",
+        color=color.rgb(
+            random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+        ),
+        scale=(block_width, block_height, block_depth),
+        texture="brick",
+        position=(x_jitter, block_height / 2 + i * block_height, z_jitter),
+        rotation=(0, 0, 0),
+        Collider="box",
+    )
+
+    blocks.append(block)
+
+#have it rotate
+def update():
+    for block in blocks:
+        block.rotation_y += 20 * time.dt
+
+
+app.run()
---
+++ ursina_play.py
@@ -0,0 +1,17 @@
+# from ursina import *
+
+# def update():
+#     firstCube.rotation_y += 20 * time.dt
+
+# app = Ursina()
+
+# firstCube = Entity(model = 'cube',
+#                    color = color.rgb(0,0,255),
+#                    texture = 'brick',
+#                    position = (0,0,0),
+#                    rotation = (0,0,0),
+#                    )
+
+# firstFloor =  Entity(model=Plane(subdivisions=(3,6)), color = color.rgb(0,0,0), texture='brick', position = (0,0,0))
+
+# app.run()
