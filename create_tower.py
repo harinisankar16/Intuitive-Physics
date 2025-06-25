90e6bc4a1a4c53e7ea7243556b91acf8472a0adc
Harini Sankar <hsankar3@illinois.edu>
2025-06-25 22:44:03 +0530

    created locally stable tower!

--- create_tower.py
+++ create_tower.py
@@ -33,36 +33,55 @@ block_height = 1
 block_depth = 3
 
 # Number of blocks in the tower
-num_blocks = 3
+num_blocks = 6
 
-blocks = []
+tower = []
 
 # Build the tower
 for i in range(num_blocks):
+    if i == 0:
+        block = Entity(
+            model="cube",
+            color=color.rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+            scale=(block_width, block_height, block_depth),
+            texture="brick",
+            position=(0, block_height / 2, 0),
+            rotation=(0, 0, 0),
+            collider="box",
+        )
+        tower.append(block)
+    else:
+        while True:
+            x_jitter = random.uniform(-block_width / 2, block_width / 2)  # randomly jitter the x position for each cube
+            z_jitter = random.uniform(-block_depth / 2, block_depth / 2)  # randomly jitter the z position for each cube
 
-    x_jitter = random.uniform(-block_width / 2, block_width / 2)  # randomly jitter the x position for each cube
-    z_jitter = random.uniform(-block_depth / 2, block_depth / 2)  # randomly jitter the z position for each cube
+            new_x = tower[-1].position.x + x_jitter
+            new_z = tower[-1].position.z + z_jitter
+            new_y = block_height / 2 + i * block_height
 
-    block = Entity(
-        model="cube",
-        color=color.rgb(
-            random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-        ),
-        scale=(block_width, block_height, block_depth),
-        texture="brick",
-        position=(x_jitter, block_height / 2 + i * block_height, z_jitter),
-        rotation=(0, 0, 0),
-        Collider="box",
-    )
+            block = Entity(
+                model="cube",
+                color=color.rgb(
+                    random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+                ),
+                scale=(block_width, block_height, block_depth),
+                texture="brick",
+                position=(new_x, new_y, new_z),
+                rotation=(0, 0, 0),
+                Collider="box",
+            )
 
-    blocks.append(block)
+            if check_cog(block, tower[-1]):
+                tower.append(block)
+                break
+            else: 
+                destroy(block)
 
-    print(f"cog of block{i} is within cog of block{i-1}: {check_cog(blocks[i-1],blocks[i])}")
+
+    print(f"cog of block{i} is within cog of block{i-1}: {check_cog(tower[-1],tower[i])}")
     
-        # x_jitter = random.uniform(-block_width / 2, block_width / 2)  # randomly jitter the x position for each cube
-        # z_jitter = random.uniform(-block_depth / 2, block_depth / 2)  # randomly jitter the z position for each cube
-
-print(blocks)
+        
+# print(blocks)
 # #have it rotate
 # def update():
 #     for block in blocks:
